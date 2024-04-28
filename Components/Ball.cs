4af798aad496bf5a5376baf1190c3a7babe02363
Michael Delfino <m_delfino@outlook.com>
2024-04-28 10:20:56 -0400

    initial commit

---
+++ Components/Ball.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Is this the correct inheritance????
+
+namespace Pong.Components
+{
+    internal class Ball : GameComponent
+    {
+        public Ball(Game game) : base(game)
+        {
+
+        }
+    }
+}
